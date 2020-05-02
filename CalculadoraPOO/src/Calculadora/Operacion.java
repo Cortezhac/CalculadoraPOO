1eb1654fdfe3367cbb3e6153f9720d1bdd42dc02
Irvin Cortez <48500847+Cortezhac@users.noreply.github.com>
2020-05-02 17:05:35 -0600

    -Atributos agregados -Constructor agregado -Setter y getter agregado

--- CalculadoraPOO/src/Calculadora/Operacion.java
+++ CalculadoraPOO/src/Calculadora/Operacion.java
@@ -10,5 +10,37 @@ package Calculadora;
  * @author Admin
  */
 public class Operacion {
+    //Atributos
+    private double DatoUno;
+    private double DatoDos;
+    private char Operador;
+    // Constructor vacio
+    public Operacion(){
+        
+    }
+    // Metodos getter y setter
+    public double getDatoUno() {
+        return DatoUno;
+    }
+
+    public void setDatoUno(double DatoUno) {
+        this.DatoUno = DatoUno;
+    }
+
+    public double getDatoDos() {
+        return DatoDos;
+    }
+
+    public void setDatoDos(double DatoDos) {
+        this.DatoDos = DatoDos;
+    }
+
+    public char getOperador() {
+        return Operador;
+    }
+
+    public void setOperador(char Operador) {
+        this.Operador = Operador;
+    }
     
 }
