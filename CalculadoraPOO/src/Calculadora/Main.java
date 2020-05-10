51bdc27da8bb4f16d6a7670b14d671d16cef5069
Irvin Cortez <48500847+Cortezhac@users.noreply.github.com>
2020-05-09 22:10:06 -0600

    gitinore modificado clase principa; modificada para mostrar unmenu y genera una serie de objetos

--- CalculadoraPOO/src/Calculadora/Main.java
+++ CalculadoraPOO/src/Calculadora/Main.java
@@ -4,17 +4,42 @@
  * and open the template in the editor.
  */
 package Calculadora;
+import java.io.*;
 
 /**
  *
- * @author Admin
+ * @author Irvin Cortez
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException{
+
         mostrarMenu();
     }
     
-    public static void mostrarMenu(){
+    public static void mostrarMenu()throws IOException{
+        int opcion;
+        BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("*****************");
+        System.out.println("Suma        1");
+        System.out.println("Restar      2");
+        System.out.println("Multiplicar 3");
+        System.out.println("Dividir     4");
+        System.out.println("*****************");
         
+        
+        switch(opcion = leer.read()){
+            case 1 :
+                Suma suma = new Suma();
+                break;
+            case 2 :
+                Resta resta = new Resta();
+                break;
+            case 3 :
+                Multiplicacion suma = new Multipliacion();
+                break;
+            case 4 :
+                Division suma = new Division();
+                break;
+        }
     }
 }
